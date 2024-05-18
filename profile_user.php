ef548318d2011adb6d5f47aa3a91a374fd322ed0
Edison De Jesus Abreu <edisondja@gmail.com>
2024-05-18 13:22:07 -0400

    Datos del single board

--- profile_user.php
+++ profile_user.php
@@ -1,17 +1,16 @@
 <?php
-    
     require('bootstrap.php');
- 
+    require('models/User.php');
+    require('models/Board.php');
 
-
-    //Load profile user;
     //cargar_tableros($id_usuario='general',$opcion='json')
 
+    $smarty->assign('content_config','profile');
     $smarty->assign('titulo',"Profile by user ".NAME_SITE);
     $smarty->assign('descripcion',NAME_SITE." plataform free for alls share your contents");
     $smarty->assign('og_imagen',LOGOSITE);
     
-    $smarty->assign('url_board',"$dominio");
+    $smarty->assign('url_board',"$dominio/controllers/");
 
 
     if(isset($_GET['user'])){
@@ -23,10 +22,7 @@
         $boards = new Board();
         $data = $boards->cargar_tableros($get_user_id,'objects');
         //print_r( $data_user);
-        $smarty->assign('content_config','profile');
         $smarty->assign('boards',$data);
-        $smarty->assign('name',$data_user->foto_url);
-        $smarty->assign('og_imagen',$data_user->foto_url);
         $smarty->assign('data_profile',$data_user);
         $smarty->display('../template/header.tpl');
 
